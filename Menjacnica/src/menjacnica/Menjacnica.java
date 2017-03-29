ac413dfa411e73dc2930cc4b1ecb30b8984afe2c
IgorSkiljevic <igorskiljevic@gmail.com>
2017-03-29 18:50:49 +0200

    Implementirane su metode u klasi Menjacnica. Izmenjen je ulazni parametar metode setSrednji, koji je ranije bio lose definisan.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,28 +1,82 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfejsi.InterfaceMenjacnica;
 import kurs.Kurs;
 import valuta.Valuta;
 
-public class Menjacnica implements InterfaceMenjacnica{
+public class Menjacnica implements InterfaceMenjacnica {
+
+	private LinkedList<Valuta> valute = new LinkedList<>();
 
 	@Override
 	public void dodajKursValute(Valuta valuta, GregorianCalendar datum, double prodajni, double kupovni) {
-		// TODO Auto-generated method stub
-		
+		Kurs k = new Kurs();
+		try {
+			k.setDatum(datum);
+			k.setKupovni(kupovni);
+			k.setProdajni(prodajni);
+			k.setSrednji();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		for (int i = 0; i < valute.size(); i++) {
+			Valuta val = valute.get(i);
+
+			if (val.equals(valuta)) {
+
+				try {
+					LinkedList<Kurs> kursevi = val.getKursevi();
+					kursevi.add(k);
+
+					val.setKursevi(kursevi);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				break;
+			}
+		}
 	}
 
 	@Override
 	public void obrisiKursValute(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		
+		for (int i = 0; i < valute.size(); i++) {
+			Valuta v = valute.get(i);
+			
+			if (v.equals(valuta)) {
+				
+				for (int j = 0; j < v.getKursevi().size(); j++) {
+					Kurs kurs = v.getKursevi().get(j);
+					
+					if (kurs.getDatum().equals(datum)) {
+						v.getKursevi().remove(j);
+					}
+				}
+			}
+		}
 	}
 
 	@Override
 	public Kurs pronadjiIVratiKursValute(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < valute.size(); i++) {
+			Valuta v = valute.get(i);
+			
+			if (v.equals(valuta)) {
+				
+				for (int j = 0; j < v.getKursevi().size(); j++) {
+					Kurs kurs = v.getKursevi().get(j);
+					
+					if (kurs.getDatum().equals(datum)) {
+						return kurs;
+					}
+				}
+			}
+		}
+		
 		return null;
 	}
 
